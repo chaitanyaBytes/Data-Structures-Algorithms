b09b839174b2c61923b37110a47e33d3d01f451c
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-04-23 02:54:53 +0530

    learn more hashing

--- hashing/hashmap.cpp
+++ hashing/hashmap.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <map>
+#include<unordered_map>
 using namespace std;
+
+
+//create a hashmap using map
+map<int, int> hashing(int n, vector<int> &v)
+{
+    // pre compute
+    map<int, int> hashmap;
+    for (int i = 0; i < n; i++)
+    {
+        hashmap[v[i]]++;
+    }
+
+    // iterate over map
+    for (auto it : hashmap)
+    {
+        cout << it.first << "->" << it.second << endl;
+    }
+
+    return hashmap;
+}
+
+
+/*problem: find and return the highest and lowest frequency elements. 
+If there are multiple elements that have the highest frequency or lowest frequency, pick the smallest element.
+approach: first create element - freq map. Initialize four variables, ‘maxFreq’, ‘minFreq’, 
+‘maxElement’, and ‘minElement’, initially assigned to 0, n, 0, and 1e9 + 1, respectively.
+iterate thru map and update maxfreq to it.second and maxelement to it.first if it.second > maxfreq.
+else if maxfreq == it.second, then update maxelement to min of maxelement and it.first.
+*/
+vector<int> getFrequencies(vector<int> &v) {
+  unordered_map<int, int> hashmap;
+
+  for (int i = 0; i < v.size(); i++) {
+    hashmap[v[i]]++;
+  }
+
+  int maxf = 0, maxk = 0, mink = 0;
+  int minf = v.size();
+  for (auto it : hashmap) {
+    if (it.second > maxf) {
+      maxf = it.second;
+      maxk = it.first;
+    } else if (maxf == it.second) {
+      maxk = min(it.first, maxk);
+    }
+  }
+
+  for (auto it : hashmap) {
+    if (it.second < minf) {
+      minf = it.second;
+      mink = it.first;
+    } else if (minf == it.second) {
+      mink = min(it.first, mink);
+    }
+  }
+
+  return {maxk, mink};
+}
+
 
 int main()
 {
@@ -13,12 +73,7 @@ int main()
         cin >> arr[i];
     }
 
-    // pre commpute
-    unordered_map<int, int> mpp;
-    for (int i = 0; i < n; i++)
-    {
-        mpp[arr[i]]++;
-    }
+    map<int, int> hashmap = hashing(n, arr);
 
     int q;
     cin >> q;
@@ -27,7 +82,7 @@ int main()
         int number;
         cin >> number;
         // fetch
-        cout << mpp[number] << endl;
+        cout << hashmap[number] << endl;
     }
     return 0;
 }

04b209f2ccbce894842917232ecfdc50f11e7b04
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-05-21 23:51:30 +0530

    array hard

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -965,6 +965,23 @@ vector<int> findTwoElement(vector<int> arr, int n)
     return {one, zero};
 }
 
+/*max subarray product*/
+int maxProduct(vector<int> &nums)
+{
+    int maxProd = INT_MIN, prefix = 1, suffix = 1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (prefix == 0)
+            prefix = 1;
+        if (suffix == 0)
+            suffix = 1;
+        prefix *= nums[i];
+        suffix *= nums[nums.size() - i - 1];
+        maxProd = max(maxProd, max(suffix, prefix));
+    }
+    return maxProd;
+}
+
 int main()
 {
     int n;

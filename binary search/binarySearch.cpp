3b9341ff7c7c461e97998836ec86c2c8cd3a3a8d
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-05-22 18:47:56 +0530

    binary search problems

--- binary search/binarySearch.cpp
+++ binary search/binarySearch.cpp
@@ -95,6 +95,95 @@ pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x)
     return {a[high], a[low]};
 }
 
+/*first and lst occurenece of an element in an array
+approach: first occurence is lower bound amd last is upper bound - 1*/
+int lowerBound(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] >= target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+int upperBound(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+vector<int> searchRange(vector<int> &nums, int target)
+{
+    int l = lowerBound(nums, target);
+    int u = upperBound(nums, target);
+    if (l == nums.size() || nums[l] != target)
+    {
+        return {-1, -1};
+    }
+    return {l, u - 1};
+}
+
+/*search in sorted but rotated array
+approach: for every index, one of the 2 halves will always be sorted. 
+First, we identify the sorted half of the array.
+Once found, we determine if the target is located within this sorted half.
+If not, we eliminate that half from further consideration. else eliminate other half
+*/
+int search(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        if (nums[low] <= nums[mid])
+        {
+            if (nums[low] <= target && target < nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (nums[mid] < target && target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     int n;

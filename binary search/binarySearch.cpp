2dc81cb1733da4019626c4150c5970b91c149f66
chaitanyaBytes <chaitanyagupta172004@gmail.com>
2024-05-23 17:16:16 +0530

    binary search problems

--- binary search/binarySearch.cpp
+++ binary search/binarySearch.cpp
@@ -234,7 +234,9 @@ bool searchDuplicates(vector<int> &arr, int k)
     return false;
 }
 
-/*find min in rotated sorted array*/
+/*find min in rotated sorted array
+Approach: if both sides of an array are sorted then the leftmost index has the minimum value.
+*/
 int findMin(vector<int> &nums)
 {
     int low = 0, high = nums.size() - 1;
@@ -269,6 +271,105 @@ int findMin(vector<int> &nums)
     //     }
     // }
     // return nums[low];
+}
+
+/*find how many times array is rotated*/
+int findKRotation(int nums[], int n)
+{
+    int low = 0, high = n - 1;
+    while (low < high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > nums[high])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+    return low;
+}
+
+/*find single element in sorted row
+Approach: in left half of single element, indices of duplicate elements are (even, odd)
+but in right half they are (odd, even). eleminate the half in which single element may
+not be present*/
+int singleNonDuplicate(vector<int> &nums)
+{
+    int low = 1, high = nums.size() - 2;
+    int n = nums.size();
+    if (n == 1)
+        return nums[0];
+    else if (nums[0] != nums[1])
+        return nums[0];
+    else if (nums[n - 1] != nums[n - 2])
+        return nums[n - 1];
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        {
+            return nums[mid];
+        }
+
+        if ((nums[mid] == nums[mid - 1] && mid % 2 == 1) ||
+            (nums[mid] == nums[mid + 1] && mid % 2 == 0))
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+/*find peak element
+Approach: peak is at i if nums[i - 1] < nums[i] > nums[i + 1].
+if nums[i] > nums[i + 1], it means it is an increasing curve and left half is eliminated.
+else right half id eliminated. */
+int findPeakElement(vector<int> &nums)
+{
+    if (nums.size() == 1 || nums[0] > nums[1])
+        return 0;
+    if (nums[nums.size() - 1] > nums[nums.size() - 2])
+        return nums.size() - 1;
+
+    int low = 1, high = nums.size() - 2;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            return mid;
+
+        if (nums[mid] > nums[mid - 1])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+
+    // linear search
+    // int n = nums.size();
+    // if (n == 1 || nums[0] > nums[1])
+    //     return 0;
+    // if (nums[n - 1] > nums[n - 2])
+    //     return n - 1;
+    // for (int i = 1; i < n - 1; i++)
+    // {
+    //     if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+    //     {
+    //         return i;
+    //     }
+    // }
 }
 
 int main()
